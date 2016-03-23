a44803fe9e38672b11df69ec49b05bb909891d58
josselaer <josselaer@smu.edu>
2016-03-23 18:02:43 +0000

    dev env working

--- api/src/routes.php
+++ api/src/routes.php
@@ -1,10 +1,17 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/Hello', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/goodbye', 
+	function($request,$response,$args) {
+	    return $response->write("Time to go. Goodbye!");
+	}
+);
+
