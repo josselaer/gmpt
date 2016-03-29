ddf85e83d0b72716548484b15940582384e91462
Aakash Patel <patela@smu.edu>
2016-03-29 12:40:17 -0500

    Pre-Flight Fix... Hopefully...

--- api/src/middleware.php
+++ api/src/middleware.php
@@ -12,7 +12,6 @@ $mw = (function ($request, $response, $next) {
 	$response = $next($request, $response);
 	$response= $response->withHeader('Access-Control-Allow-Origin','*');
 	$response= $response->withHeader('Access-Control-Allow-Methods','GET, POST, PUT, OPTIONS');
-	$response= $response->withHeader('Access-Control-Allow-Credentials','false');
 	$response= $response->withHeader('Access-Control-Allow-Headers','X-PINGOTHER');
       
 	
