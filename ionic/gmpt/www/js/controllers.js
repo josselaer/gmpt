5c6d7732740197004c11ca8e58ac69b834286c6b
Henry Warren <henrysdev@gmail.com>
2016-04-01 18:34:22 -0500

    pulls meeting list from apiary successfully. Still have yet to be able to post new meetings to the apiary.

--- ionic/gmpt/www/js/controllers.js
+++ ionic/gmpt/www/js/controllers.js
@@ -11,36 +11,36 @@ angular.module('starter.controllers', [])
     //$scope.$on('$ionicView.enter', function(e) {
     //});
 
-    $scope.chats = Chats.all();
-    $scope.remove = function (chat) {
-        Chats.remove(chat);
-    };
+$scope.chats = Chats.all();
+$scope.remove = function (chat) {
+  Chats.remove(chat);
+};
 })
 
 .controller('MessageCtrl', function ($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.messageId);
+  $scope.chat = Chats.get($stateParams.messageId);
 })
 
 .controller('LoginCtrl', function ($scope, $state, $http, Debug) {
 
-    $scope.logInfo = {};
+  $scope.logInfo = {};
 
-    $scope.login = function () {
+  $scope.login = function () {
 
-        console.log("LOGIN user: " + $scope.logInfo.userName + " - PW: " + $scope.logInfo.password);
+    console.log("LOGIN user: " + $scope.logInfo.userName + " - PW: " + $scope.logInfo.password);
 
-        $http({
-            method: "POST",
-            url: Debug.getURL("/login"),
-            data: $scope.logInfo
-        }).then(function successCallback(response) {
-            console.log("You logged in!")
-            $state.go("groups");
-        }, function errorCallback(response) {
-            alert.log("Can't Login");
-        });
+    $http({
+      method: "POST",
+      url: Debug.getURL("/login"),
+      data: $scope.logInfo
+    }).then(function successCallback(response) {
+      console.log("You logged in!")
+      $state.go("groups");
+    }, function errorCallback(response) {
+      alert.log("Can't Login");
+    });
 
-    }
+  }
 
 
 })
@@ -48,33 +48,32 @@ angular.module('starter.controllers', [])
 
 .controller('MeetingsCtrl', function($scope, $state, $http, Meetings, Debug) {
 
-/*
+
   $http({
 
-        method: "GET",
-        url: Debug.getURL("/meetings"),
-        responseType: "json"
-      }).then(function successCallback(response) {
+    method: "GET",
+    url: Debug.getURL("/meetings"),
+    responseType: "json"
+  }).then(function successCallback(response) {
 
-        console.log(Debug.getURL("/meetings"));
-        console.log(response);
-        meetings = response.data;
-        console.log(meetings);
+    console.log(Debug.getURL("/meetings"));
+    console.log(response);
+    meetings = response.data;
+    console.log(meetings);
 
-        Meetings.set(meetings);
-        $scope.meetings = meetings;
+    Meetings.set(meetings);
+    $scope.meetings = meetings;
     
-      }, function errorCallback(response) {
+  }, function errorCallback(response) {
 
-        console.log(Debug.getURL("/meetings"));
-        console.log(response);
+    console.log(Debug.getURL("/meetings"));
+    console.log(response);
 
-        alert("Failed to load groups, please try again.");
+    alert("Failed to load groups, please try again.");
 
-        return null;
 
-      });
-*/
+  });
+
   $scope.meetings = Meetings.all();
   //console.log("current index outside function: ", Meetings.getCurr());
   $scope.currentMeeting = Meetings.get(Meetings.getCurr());
@@ -98,11 +97,11 @@ angular.module('starter.controllers', [])
 
   $scope.confirmMeeting = function()
   {  
-    if(this.date != "" && this.time_ != "" && this.topic != "" && this.desc != "")
+    if(this.date != "" && this.startTime != "" && this.topic != "" && this.desc != "")
     {
       if(Meetings.getEdit() == false)
       {
-        $scope.meetings.push({'date':this.date,'time_':this.time_,'topic':this.topic,'desc':this.desc});
+        $scope.meetings.push({'date':this.date,'startTime':this.startTime,'topic':this.topic,'desc':this.desc});
         Meetings.set($scope.meetings);
       }
       else if(Meetings.getEdit() == true)
@@ -110,12 +109,28 @@ angular.module('starter.controllers', [])
         console.log("lol");
         $scope.meetings[Meetings.getCurr()].topic = this.topic;
         $scope.meetings[Meetings.getCurr()].date = this.date;
-        $scope.meetings[Meetings.getCurr()].time_ = this.time_;
+        $scope.meetings[Meetings.getCurr()].startTime = this.startTime;
         $scope.meetings[Meetings.getCurr()].desc = this.desc;
         Meetings.set($scope.meetings);
+        var meeting
+        {
+          topic = this.topic;
+          date = this.date;
+          startTime = this.startTime;
+          description = this.desc;
+        }
+        $http({
+          method:"POST",
+          url: Debug.getURL("/meetings"),
+          data: meeting
+        }).then(function successCallback(response) {
+          //$state.go("groups");
+        }, function errorCallback(response) {
+          alert.log("Failed to add group");
+        });
       }
       this.date = "";
-      this.time_ = "";
+      this.startTime = "";
       this.topic = "";
       this.desc = "";
       /*
@@ -123,24 +138,24 @@ angular.module('starter.controllers', [])
       {
         return !item.done;
       })*/
-    }
+}
 
-    console.log($scope.meetings);
-  }
+console.log($scope.meetings);
+}
 
-  $scope.editMeeting = function(index)
-  {
-    Meetings.setEdit(true);
-    Meetings.setCurr(index);
-    $scope.currentMeeting = Meetings.get(Meetings.getCurr());
-  }
+$scope.editMeeting = function(index)
+{
+  Meetings.setEdit(true);
+  Meetings.setCurr(index);
+  $scope.currentMeeting = Meetings.get(Meetings.getCurr());
+}
 
-  $scope.newMeeting = function()
-  {
-    Meetings.setEdit(false);
-    Meetings.setCurr(999);
-    $scope.currentMeeting = Meetings.get(Meetings.getCurr());
-  }
+$scope.newMeeting = function()
+{
+  Meetings.setEdit(false);
+  Meetings.setCurr(999);
+  $scope.currentMeeting = Meetings.get(Meetings.getCurr());
+}
 })
 
 .controller('AddMeetingCtrl', function($scope, $state, $http, Debug) 
@@ -152,29 +167,29 @@ angular.module('starter.controllers', [])
 
   $http({
 
-        method: "GET",
-        url: Debug.getURL("/groups"),
-        responseType: "json"
-      }).then(function successCallback(response) {
+    method: "GET",
+    url: Debug.getURL("/groups"),
+    responseType: "json"
+  }).then(function successCallback(response) {
 
-        console.log(Debug.getURL("/groups"));
-        console.log(response);
-        groups = response.data;
-        console.log(groups);
+    console.log(Debug.getURL("/groups"));
+    console.log(response);
+    groups = response.data;
+    console.log(groups);
 
-        Groups.set(groups);
-        $scope.groups = groups;
+    Groups.set(groups);
+    $scope.groups = groups;
     
-      }, function errorCallback(response) {
+  }, function errorCallback(response) {
 
-        console.log(Debug.getURL("/groups"));
-        console.log(response);
+    console.log(Debug.getURL("/groups"));
+    console.log(response);
 
-        alert("Failed to load groups, please try again.");
+    alert("Failed to load groups, please try again.");
 
-        return null;
+    return null;
 
-      });
+  });
 
 
   $scope.groups = Groups.all();
@@ -182,34 +197,34 @@ angular.module('starter.controllers', [])
 
 .controller('AddGroupCtrl', function($scope, $state, $http, Debug) {
 
-    $scope.group = {};
+  $scope.group = {};
 
-    $scope.search = '';
-    $scope.orderByAttribute = '';
-    $scope.members = [
-    {"_username":"henrysdev","_email":"henrysdev@gmail.com","done":false,"remove":false}
-    ];
-    $scope.addMember = function()
+  $scope.search = '';
+  $scope.orderByAttribute = '';
+  $scope.members = [
+  {"_username":"henrysdev","_email":"henrysdev@gmail.com","done":false,"remove":false}
+  ];
+  $scope.addMember = function()
+  {
+    console.log("Clicked");
+    console.log("username: ", this._username, "email: ", this._email);
+    if(this._username !=' ' && this._email !=' ')
     {
-      console.log("Clicked");
-      console.log("username: ", this._username, "email: ", this._email);
-      if(this._username !=' ' && this._email !=' ')
-      {
-        $scope.members.push({'_username':this._username,'_email':this._email,'done':false, 'remove':false});
-        this._username = ' ';
-        this._email = ' ';
-      }
+      $scope.members.push({'_username':this._username,'_email':this._email,'done':false, 'remove':false});
+      this._username = ' ';
+      this._email = ' ';
     }
-    
-    $scope.removeItem = function(index)
+  }
+
+  $scope.removeItem = function(index)
+  {
+    $scope.members.splice(index,1);
+    console.log("delete member");
+    $scope.members = $scope.members.filter(function(item)
     {
-      $scope.members.splice(index,1);
-      console.log("delete member");
-      $scope.members = $scope.members.filter(function(item)
-      {
-        return !item.done;
-      })
-    }
+      return !item.done;
+    })
+  }
 
   $scope.addGroup = function() {
 
