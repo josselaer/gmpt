9ce4fefe564a297e7f79ad7fe34af79622dba448
josselaer <josselaer@smu.edu>
2016-04-18 00:27:56 +0000

    fake data

---
+++ database/fake_data.sql
@@ -0,0 +1,15 @@
+USE GMPT;
+
+CALL CreateProject('Team Titans','We are making a cool website','2016-04-13');
+CALL CreateProject('Networks Group','Network Research Paper','2016-04-13');
+CALL CreateProject('Algorithms Group','We make algorithms','2016-04-13');
+
+CALL CreateMeeting('143','Paper Prototypes','2016-04-15','Innovation Gym','12:00:00','13:00:00');
+CALL CreateMeeting('143','Work on Login and Register','2016-04-17','Caruth','12:00:00','13:00:00');
+CALL CreateMeeting('143','Finish the website','2016-04-22','Embrey','12:00:00','13:00:00');
+CALL CreateMeeting('144','Do research proposal','2016-04-17','Fondren','12:00:00','13:00:00');
+CALL CreateMeeting('144','First Draft','2016-04-17','Network Room','12:00:00','13:00:00');
+CALL CreateMeeting('144','Final Presentation','2016-04-25','Library','12:00:00','13:00:00');
+CALL CreateMeeting('145','Djikstras','2016-04-16','Innovation Gym','12:00:00','13:00:00');
+CALL CreateMeeting('145','Start Presentation','2016-04-22','TBD','12:00:00','13:00:00');
+CALL CreateMeeting('145','Practice presentation','2016-04-30','Innovation Gym','12:00:00','13:00:00');
